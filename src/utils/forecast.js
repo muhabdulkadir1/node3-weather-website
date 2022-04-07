f5f1afa7a855cd92666f109d3169acb2b77ed02b
Babban Gona Lagos <muhammed.abdulkadir@babbangona.com>
2022-04-07 07:44:18 +0100

    Initial commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,19 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=7c59f183cb5824cbe5712e7c406ddd97&query='+ latitude + ',' + longitude + '&unit=f'
+
+    request(
+        {url, json: true}, (error, {body}) => {
+            if (error) {
+                callback("Unable to connect to weatherstack")
+            } else if (body.error) {
+                callback("Unable to connect to find location")
+            } else {
+                callback(undefined, body.current.weather_descriptions[0] + " throughout the day. It is currently " + body.current.temperature + " degrees out. There is a " + body.current.feelslike + "% chance of rain.")
+            } 
+        }
+    )
+}
+
+module.exports = forecast
